79b47e84a4d6a908de0a8dcb6e2e5e8ba3ff92e8
lintao <lintao.zhang@thoughtworks.com>
2020-10-22 21:13:30 +0800

    [Lintao] fix: group again add duplicated students

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/Service/GroupService.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/Service/GroupService.java
@@ -6,6 +6,7 @@ import com.thoughtworks.capability.gtb.restfulapidesign.repository.GroupReposito
 import com.thoughtworks.capability.gtb.restfulapidesign.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,8 +16,10 @@ public class GroupService {
     private final StudentRepository studentRepository = new StudentRepository();
 
     public List<Group> randomlyGroupStudents() {
+        // TODO should I use studentRepository in this place? or I should use studentService in groupController and pass students data to this method
         List<Student> students = studentRepository.getStudents();
         List<Group> groups = groupRepository.findAll();
+        groups.forEach(group -> group.setStudents(new ArrayList<>()));
         Collections.shuffle(students);
         for (int i = 0; i < students.size(); i++) {
             groups.get(i % groups.size()).getStudents().add(students.get(i));
