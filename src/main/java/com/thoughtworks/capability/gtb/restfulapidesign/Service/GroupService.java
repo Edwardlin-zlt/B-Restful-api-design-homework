09c6f5dc57dafbf8b6e6a2ac4e08b718c0a6b4f6
lintao <lintao.zhang@thoughtworks.com>
2020-10-22 12:10:50 +0800

    [Lintao] refact: student repository

---
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/Service/GroupService.java
@@ -0,0 +1,2 @@
+package com.thoughtworks.capability.gtb.restfulapidesign.Service;public class GroupServiece {
+}
--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/repository/StudentRepository.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/repository/StudentRepository.java
@@ -4,57 +4,45 @@ import com.thoughtworks.capability.gtb.restfulapidesign.Exceptions.StudentNotExi
 import com.thoughtworks.capability.gtb.restfulapidesign.model.Gender;
 import com.thoughtworks.capability.gtb.restfulapidesign.model.Student;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StudentRepository {
     private static Integer id = 0;
-    private final List<Student> students = new ArrayList<Student>() {
-        {
-            add(new Student(1, "stu1", Gender.MALE, "haha"));
-            add(new Student(2, "stu2", Gender.MALE, "heihei"));
-            add(new Student(3, "stu3", Gender.FEMALE, "huohuo"));
-        }
-    };
+    private final static Map<Integer, Student> studentMap = new HashMap<>();
+    static {
+        studentMap.put(++id, new Student(id, "stu1", Gender.MALE, "haha"));
+        studentMap.put(++id, new Student(id, "stu2", Gender.MALE, "haha"));
+        studentMap.put(++id, new Student(id, "stu3", Gender.MALE, "haha"));
+    }
 
     public List<Student> getStudents() {
-        return students;
+        return new ArrayList<>(studentMap.values());
     }
 
     public Student addStudent(Student student) {
         student.setId(++id);
-        students.add(student);
+        studentMap.put(id, student);
         return student;
     }
 
-    public void deleteById(int id) throws StudentNotExistException {
-        students.remove(findById(id));
+    public void deleteById(int id) {
+        studentMap.remove(id);
     }
 
     public List<Student> getByGender(Gender gender) {
-        return students.stream().filter(student -> student.getGender().equals(gender)).collect(Collectors.toList());
+        return studentMap.values().stream()
+                .filter(student -> student.getGender().equals(gender))
+                .collect(Collectors.toList());
     }
 
-    public Student findById(int id) throws StudentNotExistException {
-        return students.stream().filter(student -> student.getId().equals(id)).findFirst()
-                .orElseThrow(() -> new StudentNotExistException("用户不存在"));
+    public Optional<Student> findById(int id) {
+        return studentMap.values().stream()
+                .filter(student -> student.getId().equals(id))
+                .findFirst();
     }
 
-    public Student updateById(int id, Student student) throws StudentNotExistException {
-        Student studentFindById = findById(id);
-        students.remove(studentFindById);
-        studentFindById.setName(student.getName());
-        studentFindById.setGender(student.getGender());
-        studentFindById.setNote(student.getNote());
-        students.add(studentFindById);
-        return studentFindById;
-    }
-
-    public List<Student> getShuffledStudents() {
-        ArrayList<Student> newStudents = new ArrayList<>(this.students);
-        Collections.shuffle(newStudents);
-        return newStudents;
+    public void update(Student student) {
+        studentMap.put(student.getId(), student);
     }
 }
